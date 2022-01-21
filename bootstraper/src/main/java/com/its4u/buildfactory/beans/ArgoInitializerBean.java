47663a13f30621f586f0872b910f2aeca41d7481
kevyn.schrondweiler <kevyn.schrondweiler@ITS4U173.mshome.net>
2022-01-21 17:55:02 +0100

    load yaml

--- bootstraper/src/main/java/com/its4u/buildfactory/beans/ArgoInitializerBean.java
+++ bootstraper/src/main/java/com/its4u/buildfactory/beans/ArgoInitializerBean.java
@@ -1,8 +1,13 @@
 package com.its4u.buildfactory.beans;
 
+import java.util.List;
+
 import org.primefaces.model.TreeNode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.yaml.snakeyaml.Yaml;
+
+import com.its4u.buildfactory.ocp.resources.TemplateResource;
 
 import lombok.Data;
 
@@ -18,4 +23,24 @@ public class ArgoInitializerBean {
 	
 	@Value("${argo.password}")
 	private String argoPassword;
+	
+	public void createInfra(List<TemplateResource> resources) {
+		
+		for (TemplateResource template:resources) {
+			if (template.getName().startsWith("00-")) {
+				System.out.println("create "+template.getName());
+				parseCreatedRessource(template.getResourceAsString());
+			}
+		}
+		
+	}
+	
+	private void parseCreatedRessource(String resourceyml) {
+		System.out.println(resourceyml);
+		Yaml yaml = new Yaml();
+		for (Object object : yaml.loadAll(resourceyml)) {
+			System.out.println(object.toString());
+		}
+		
+	}
 }
--- bootstraper/src/main/java/com/its4u/buildfactory/ocp/resources/TemplateResource.java
+++ bootstraper/src/main/java/com/its4u/buildfactory/ocp/resources/TemplateResource.java
@@ -1,11 +1,7 @@
 package com.its4u.buildfactory.ocp.resources;
 
-import java.util.HashMap;
-import java.util.List;
-
 import org.apache.commons.lang3.StringUtils;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
