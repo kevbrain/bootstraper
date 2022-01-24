4e68aa13ef493a3e16b9a12fc6a90886301568be
kevyn.schrondweiler <kevyn.schrondweiler@ITS4U173.mshome.net>
2022-01-24 14:28:01 +0100

    fiw

--- bootstraper/src/main/java/com/its4u/buildfactory/beans/GitInitializerBean.java
+++ bootstraper/src/main/java/com/its4u/buildfactory/beans/GitInitializerBean.java
@@ -258,11 +258,20 @@ public class GitInitializerBean {
 				} finally {
 					pushCommand.call();
 				}
-			    pollView.log("Git Project created");	
+			    int j=0;
+			    System.out.println("Git Project created");	
 			    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Project Git created"));
+			    System.out.println("Wait 10s ....");
+		    	for (int i=0;i<30000;i++) {j++;};
+		    	System.out.println("End Wait ...."+j);
 			    createArgoApp();
 			    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Application created in GitOps"));
+			    System.out.println("Argo application created");
+			    System.out.println("Wait 10s ....");
+		    	for (int i=0;i<30000;i++) {j++;};
+		    	System.out.println("End Wait ...."+j);
 			    createGitAppsDeploy(project);
+			    System.out.println("APPS deploy created");
 			    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Project created in GitOpsApp"));
 		} catch (Exception e) {
 			e.printStackTrace();
