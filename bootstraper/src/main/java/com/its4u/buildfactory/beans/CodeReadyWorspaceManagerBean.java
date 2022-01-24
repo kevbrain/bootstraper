27b9b91c175ee50c6d9211a371dd2a1a7f12a91a
kevyn.schrondweiler <kevyn.schrondweiler@ITS4U173.mshome.net>
2022-01-24 18:34:28 +0100

    codeready

--- bootstraper/src/main/java/com/its4u/buildfactory/beans/CodeReadyWorspaceManagerBean.java
+++ bootstraper/src/main/java/com/its4u/buildfactory/beans/CodeReadyWorspaceManagerBean.java
@@ -1,6 +1,7 @@
 package com.its4u.buildfactory.beans;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
@@ -8,5 +9,23 @@ import lombok.Data;
 @Data
 @Component
 public class CodeReadyWorspaceManagerBean {
+	
+	@Value("${codeready.user}")
+	private String user;
+	
+	@Value("${codeready.client.id}")
+	private String clientId;
+	
+	@Value("${codeready.access.token.url}")
+	private String authProviderUrl;
+	
+	@Value("${codeready.workspace.url}")
+	private String workspaceBaseUrl;
+	
+	@Value("${codeready.user.password}")
+	private String userPassword;
+	
+	@Value("${codeready.client.secret}")
+	private String clientSecret;
 
 }
